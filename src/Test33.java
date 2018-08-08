b11fee0d9f2b332c6edf278dcc289f9825cfb08b
daran <sjtuhuangyong@sjtu.edu.cn>
2018-08-08 17:19:42 +0800

    update

--- src/Test33.java
+++ src/Test33.java
@@ -1,45 +1,35 @@
-import org.junit.Test;
-
-import java.lang.management.GarbageCollectorMXBean;
-
 /*
 丑数
 题目描述
 把只包含质因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含质因子7。 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
  */
+
 public class Test33 {
     public int GetUglyNumber_Solution(int index) {
-        if (index == 1) {
+        if (index <= 0)
+            throw new IllegalArgumentException("index should be greater than zero");
+        if (index == 1)
             return 1;
-        }
-        int count = 1;
-        for (int i = 2;;i++) {
-            if (isUgly(i)) {
-                count++;
+        int[] memo = new int[index];
+        memo[0] = 1;
+        int m2 = 0;
+        int m3 = 0;
+        int m5 = 0;
+        for (int i = 1; i<index; i++) {
+            int min = Math.min(memo[m2]*2, Math.min(memo[m3]*3, memo[m5]*5));
+            memo[i] = min;
+            //找第1个*2大于之前最小值的数，作为下一轮的备选
+            while (memo[m2]*2 <= min) {
+                m2++;
             }
-            if (count == index) {
-                return i;
+            while (memo[m3]*3 <= min) {
+                m3++;
+            }
+            while (memo[m5]*5 <= min) {
+                m5++;
             }
         }
-    }
-
-    public boolean isUgly(int num) {
-        if (num <= 0)
-            return false;
-
-        while(num % 2 == 0) {
-            num /= 2;
-        }
-        while (num % 3 == 0) {
-            num /= 3;
-        }
-        while (num % 5 == 0) {
-            num /= 5;
-        }
-
-        if (num == 1)
-            return true;
-        return false;
+        return memo[index-1];
     }
 
 }
