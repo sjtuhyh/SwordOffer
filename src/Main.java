dc0470bb0b03f111a88c49c1af6d08cec8689b60
daran <sjtuhuangyong@sjtu.edu.cn>
2018-08-09 17:49:51 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,43 @@
 public class Main {
 
-    public static void quickSort(int[] arr) {
+    private static int count = 0;
+    public static void mergeSort(int[] arr) {
         if (arr == null || arr.length < 2)
             return;
-        quickSort(arr, 0, arr.length-1);
+        mergeSort(arr, 0, arr.length-1);
     }
 
-    private static void quickSort(int[] arr, int l, int r) {
-        if (l < r) {
-            swap(arr, l+(int)Math.random()*(r-l+1), r);
-            int[] p = partition(arr, l, r);
-            quickSort(arr, l, p[0]-1);
-            quickSort(arr, p[1]+1, r);
+    private static void mergeSort(int[] arr, int l, int r) {
+        if (l == r)
+            return;
+        int mid = l + (r-l)>>1;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r-l+1];
+        int p1 = l;
+        int p2 = mid+1;
+        int i = 0;
+        while (p1 <= mid && p2 <= r) {
+            if (arr[p1] > arr[p2]) {
+                help[i++] = arr[p2++];
+            } else {
+                help[i++] = arr[p1++];
+            }
         }
-    }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
 
-
-
-
-    public static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
+        for (int k = 0; k<help.length; k++) {
+            arr[k+l] = help[k];
+        }
     }
 
 
--- src/Test35.java
+++ src/Test35.java
@@ -26,14 +26,41 @@ public class Test35 {
     public int InversePairs(int [] array) {
         if (array == null || array.length < 2)
             return 0;
-        return InversePairs(array, 0, array.length-1);
+        long res = InversePairs(array, 0, array.length-1);
+        return (int)(res%1000000007);
     }
 
-    private int InversePairs(int[] array, int l, int r) {
-        if (l < r) {
-            int mid = l + ((r-l)>>1);
+    private long InversePairs(int[] array, int l, int r) {
+        if (l == r)
+            return 0;
+        int mid = l + ((r-l)>>1);
+        return InversePairs(array, l, mid) + InversePairs(array, mid+1, r) + merge(array, l, mid, r);
+    }
 
+    private long merge(int[] array, int l, int mid, int r) {
+        int[] help = new int[r-l+1];
+        int i = 0;
+        int p1 = l;
+        int p2 = mid+1;
+        long res = 0;
+        while (p1 <= mid && p2 <= r) {
+            if (array[p1] <= array[p2]) {
+                help[i++] = array[p1++];
+            } else {
+                res = res + mid - p1 + 1;
+                help[i++] = array[p2++];
+            }
         }
+        while (p1 <= mid) {
+            help[i++] = array[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = array[p2++];
+        }
+        for (int k = 0; k<help.length; k++) {
+            array[l+k] = help[k];
+        }
+        return res;
     }
 
 
